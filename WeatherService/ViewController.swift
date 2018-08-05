ea951c0a0075b7ede0d566d2a227b1b6f372814c
max <maxymyers@gmail.com>
2018-08-05 10:58:01 -0700

    “Initial”

---
+++ WeatherService/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherService
+//
+//  Created by david Myers on 8/5/18.
+//  Copyright © 2018 Max Myers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
